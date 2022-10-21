93c257e1cb2d24384fc892af04d3ab5f09779cef
ankit <ankitsrivastava208@gmail.com>
2022-10-21 09:18:00 +0530

    Added the corrected code

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Team from './components/About/Team';
-import Mdmessage from './components/About/Mdmessage';
-import Chairperson from './components/About/Chairperson';
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Team from "./components/About/Team";
+import Mdmessage from "./components/About/Mdmessage";
+import Chairperson from "./components/About/Chairperson";
 //import Team from './components/Home/Home.js';
-import {BrowserRouter as Router, Routes,} from "react-router-dom";
-import Director from './components/About/Director';
-import About from './components/About/About';
-import Contact from './components/Contact/Contact';
-import PreMedical from './components/PreMedical/PreMedical';
-import Pathlogy from './components/Pathlogy/Pathlogy';
-import Galleryimage from './components/Gallery/Gallery';
-import Video from './components/Gallery/Video';
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+import Director from "./components/About/Director";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import PreMedical from "./components/PreMedical/PreMedical";
+import Pathlogy from "./components/Pathlogy/Pathlogy";
+import Home from "./components/Home/Home";
+
 function App() {
   return (
     <div className="App">
+      <Header />
 
-    <Header/>
+      <Home />
+      {/* <Video/> */}
+      {/* <Galleryimage/> */}
+      <Pathlogy />
+      <PreMedical />
 
-    {/* <Home/> */}
-    <Video/>
-    <Galleryimage/>
-    <Pathlogy/>
-    <PreMedical/>
-    
-    <Contact/>
-    <About/>
-    <Director/>
-    <Chairperson/>
-    <Mdmessage/>
-<Team/>
+      <Contact />
+      <About />
+      <Director />
+      <Chairperson />
+      <Mdmessage />
+      <Team />
 
-    <Footer/>
-
+      <Footer />
     </div>
   );
 }
